386ecc8a32c437e0b3099a9a3eb4c508b0edbf7c
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-04-07 17:21:19 +0700

    deklarasi array

--- array.cpp
+++ array.cpp
@@ -2,3 +2,10 @@
 using namespace std;
 
 int main()
+{
+    //deklarasi array disertai inisialisasi
+    int dataku[5] = {10, 20, 30, 40, 50};
+    //deklarasi tanpa inisialisasi
+    int datamu[5];
+
+}
