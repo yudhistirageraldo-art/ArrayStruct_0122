5ff687fcb1a19ad04e0974b2f028ff7a43c7e2e5
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-04-07 17:28:03 +0700

    mengisi array

--- array.cpp
+++ array.cpp
@@ -10,5 +10,9 @@ int main()
 
     //cara akses (membaca atau mengambil isi array)
     cout << "Isi data kedua : " << dataku[1] << endl;
+    //mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah di ubah : " << dataku[1] << endl;
+
 
 }
