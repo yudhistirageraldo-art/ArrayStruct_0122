9ba0d07f907b26589e0d687ca71c9a3bf3bdb3c1
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-04-07 18:41:38 +0700

    mengisi data

--- structdalamstruct.cpp
+++ structdalamstruct.cpp
@@ -12,5 +12,23 @@ struct Orang
 {
     string nama;
     detailAlamat alamat;
-    int umur
+    int umur;
 };
+
+int mail()
+{
+    //deklarasi variable struct
+    Orang mhs;
+    //mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Isikan data kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    
+}
