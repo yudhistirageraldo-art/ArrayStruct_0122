ac0778cf58f153d642b0be1ead930ffa96728f30
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-04-07 18:30:29 +0700

    selesai

--- struct.cpp
+++ struct.cpp
@@ -7,3 +7,22 @@ struct Orang
     string alamat;
     int umur;
 };
+
+int main()
+{
+    //Deklarasi variabel struct
+    Orang mhs;
+    //Mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << "Data tampil" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
